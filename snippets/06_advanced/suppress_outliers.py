d10b6aac42df572cb1f40749b5a974c4f7a570d6
AI2Table <76231236+ai2table@users.noreply.github.com>
2024-03-04 12:37:20 -0800

    changes to snippets

--- snippets/06_advanced/suppress_outliers.py
+++ snippets/06_advanced/suppress_outliers.py
@@ -1,14 +1,14 @@
 def suppress_outliers(
-        df, outlier_col: list[str] | None, q1_percentile: float, q3_percentile: float,
-        result_col_prefix: str | None, result_col_suffix: str | None):
+        df, outlier_col: list[str] | None, q1_percentile: float = 0.25, q3_percentile: float = 0.75,
+        result_col_suffix: str | None = "no_outlier", result_col_prefix: str | None = None):
     """
     Suppresses outliers in specified numeric columns of the dataframe based on custom percentile values for Q1 and Q3.
-    Adds new columns with the selected prefix, or suffix. Only one of prefix or suffix can be used, not both.
+    Adds new columns with the selected suffix or prefix. If no suffix or prefix is provided, overwrites the existing column.
     :param outlier_col: List of numeric columns to check for outliers. If left empty, applies to all numeric columns.
-    :param q1_percentile: Custom percentile for Q1 (e.g., 0.20 for 20th percentile).
-    :param q3_percentile: Custom percentile for Q3 (e.g., 0.80 for 80th percentile).
-    :param result_col_prefix: Prefix for the result columns where the suppressed data will be stored.
-    :param result_col_suffix: Or, suffix for the result columns where the suppressed data will be stored.
+    :param q1_percentile: Custom percentile for Q1 (e.g., 0.25 for 25th percentile).
+    :param q3_percentile: Custom percentile for Q3 (e.g., 0.75 for 75th percentile).
+    :param result_col_suffix: Suffix for the new column where the suppressed data will be stored.
+    :param result_col_prefix: Prefix for the new column where the suppressed data will be stored.
     """
     import numpy as np
 
@@ -28,16 +28,14 @@ def suppress_outliers(
         lower_bound = q1 - 1.5 * iqr
         upper_bound = q3 + 1.5 * iqr
 
-        if result_col_prefix:
-            if result_col_suffix:
-                raise ValueError("Either suffix or prefix should be provided, not both.")
+        if result_col_suffix:
+            new_col_name = f'{col}_{result_col_suffix}'
+        elif result_col_prefix:
             new_col_name = f'{result_col_prefix}_{col}'
         else:
-            if not result_col_suffix:
-                raise ValueError("One of suffix or prefix must be provided.")
-            new_col_name = f'{col}_{result_col_suffix}'
+            new_col_name = col
 
-        # Create a new column with suppressed values
+        # Create a new column (or override), with suppressed values
         df[new_col_name] = np.where(
             df[col] < lower_bound, lower_bound, np.where(df[col] > upper_bound, upper_bound, df[col])
         )
